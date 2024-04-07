aa04f24e241dfac80dbdd0e4d9963e96860af583
aoaaceai <aoaaceai@csie.ntu.edu.tw>
2024-04-07 17:32:31 +0800

    pinout for prototype 1
    (This version does not contain the changes to pcb files)

--- fw/Core/Src/main.c
+++ fw/Core/Src/main.c
@@ -40,6 +40,7 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
+PCD_HandleTypeDef hpcd_USB_FS;
 
 /* USER CODE BEGIN PV */
 
@@ -47,13 +48,34 @@
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_USB_PCD_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+static void FlashGpio(void)
+{
+//	HAL_GPIO_TogglePin(OnBoard_GPIO_Port, OnBoard_Pin);
+//	HAL_GPIO_TogglePin(IrLed_GPIO_Port, IrLed_Pin);
+	HAL_GPIO_TogglePin(Flashing_GPIO_Port, Flashing_Pin);
+}
 
+static void SwitchAlwaysOn(void)
+{
+	HAL_GPIO_WritePin(AlwaysOn_GPIO_Port, AlwaysOn_Pin, GPIO_PIN_SET);
+}
+
+static void DetectIr(void)
+{
+	GPIO_PinState state = HAL_GPIO_ReadPin(IrSensor_GPIO_Port, IrSensor_Pin);
+	if (state)
+		HAL_UART_Transmit(&huart2, "off\r\n", 5, 1000);
+	else
+		HAL_UART_Transmit(&huart2, "on\r\n", 4, 1000);
+}
 /* USER CODE END 0 */
 
 /**
@@ -83,8 +105,10 @@ int main(void)
   /* USER CODE END SysInit */
 
   /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_USB_PCD_Init();
   /* USER CODE BEGIN 2 */
-
+  SwitchAlwaysOn();
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -106,6 +130,7 @@ void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
 
   /** Initializes the RCC Oscillators according to the specified parameters
   * in the RCC_OscInitTypeDef structure.
@@ -115,7 +140,7 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI_DIV2;
-  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL2;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL16;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
     Error_Handler();
@@ -127,13 +152,106 @@ void SystemClock_Config(void)
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
   {
     Error_Handler();
   }
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USB;
+  PeriphClkInit.UsbClockSelection = RCC_USBCLKSOURCE_PLL;
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief USB Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USB_PCD_Init(void)
+{
+
+  /* USER CODE BEGIN USB_Init 0 */
+
+  /* USER CODE END USB_Init 0 */
+
+  /* USER CODE BEGIN USB_Init 1 */
+
+  /* USER CODE END USB_Init 1 */
+  hpcd_USB_FS.Instance = USB;
+  hpcd_USB_FS.Init.dev_endpoints = 8;
+  hpcd_USB_FS.Init.speed = PCD_SPEED_FULL;
+  hpcd_USB_FS.Init.low_power_enable = DISABLE;
+  hpcd_USB_FS.Init.lpm_enable = DISABLE;
+  hpcd_USB_FS.Init.battery_charging_enable = DISABLE;
+  if (HAL_PCD_Init(&hpcd_USB_FS) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USB_Init 2 */
+
+  /* USER CODE END USB_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+/* USER CODE BEGIN MX_GPIO_Init_1 */
+/* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, IrOut_Pin|LedCh_Pin|LedCg_Pin|LedCf_Pin
+                          |LedCe_Pin|LedRc_Pin|LedRb_Pin|LedRa_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, LedCd_Pin|LedCc_Pin|LedCb_Pin|LedCa_Pin
+                          |LedRh_Pin|LedRg_Pin|LedRf_Pin|LedRe_Pin
+                          |LedRd_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : IrIn_Pin */
+  GPIO_InitStruct.Pin = IrIn_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(IrIn_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : IrOut_Pin LedCh_Pin LedCg_Pin LedCf_Pin
+                           LedCe_Pin LedRc_Pin LedRb_Pin LedRa_Pin */
+  GPIO_InitStruct.Pin = IrOut_Pin|LedCh_Pin|LedCg_Pin|LedCf_Pin
+                          |LedCe_Pin|LedRc_Pin|LedRb_Pin|LedRa_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : LedCd_Pin LedCc_Pin LedCb_Pin LedCa_Pin
+                           LedRh_Pin LedRg_Pin LedRf_Pin LedRe_Pin
+                           LedRd_Pin */
+  GPIO_InitStruct.Pin = LedCd_Pin|LedCc_Pin|LedCb_Pin|LedCa_Pin
+                          |LedRh_Pin|LedRg_Pin|LedRf_Pin|LedRe_Pin
+                          |LedRd_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+/* USER CODE BEGIN MX_GPIO_Init_2 */
+/* USER CODE END MX_GPIO_Init_2 */
 }
 
 /* USER CODE BEGIN 4 */
