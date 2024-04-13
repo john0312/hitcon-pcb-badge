b865444d2dc261c6d0173172cb0835fe651c3e53
Chi-Feng Tsai <ktpss95112@gmail.com>
2024-04-13 22:26:39 +0800

    add infrared

---
+++ game/infrared.c
@@ -0,0 +1,132 @@
+#include "infrared.h"
+
+/**
+ * Notes:
+ * - To improve performance, use xor of integers instead of inner product of
+ * array.
+ */
+
+raw_packet_t encode_packet(uint8_t size_byte, uint8_t *data) {
+  raw_packet_t packet;
+  size_t i = 0;
+  for (; i < PACKET_START_LEN; i++) {
+    packet.bits[i] = PACKET_START[i];
+  }
+  for (size_t j = 0; j < PACKET_SIZE_LEN; j++) {
+    packet.bits[i++] = (size_byte >> j) & 1; // Little-endian
+  }
+  int parity = 0;
+  for (int j = 0; j < size_byte; j++) {
+    for (int k = 0; k < 8; k++) {
+      packet.bits[i++] = (data[j] >> k) & 1; // Little-endian
+      parity ^= packet.bits[i - 1];
+    }
+  }
+  packet.bits[i++] = parity;
+  for (size_t j = 0; j < PACKET_END_LEN; j++) {
+    packet.bits[i++] = PACKET_END[j];
+  }
+  packet.size = i;
+  return packet;
+}
+
+#define QUEUE_MAX_SIZE ((PACKET_MAX_LEN + 10) * DECODE_SAMPLE_RATIO)
+typedef struct {
+  uint8_t buf[QUEUE_MAX_SIZE];
+  size_t start, end; // data is in [start, end), circular buffer
+} queue_t;
+
+// TODO: profile and see if using if-else is faster than modulo
+#define queue_size(q)                                                          \
+  (((q)->end - (q)->start + QUEUE_MAX_SIZE) % QUEUE_MAX_SIZE)
+
+// Return -1 if the bit at pos is invalid.
+// Return 1 if the bit at pos is 1.
+// Return 0 if the bit at pos is 0.
+int decode_bit_at_pos(queue_t *buf, size_t pos) {
+  int num_ones = 0;
+  for (size_t j = 0; j < DECODE_SAMPLE_RATIO; j++) {
+    num_ones +=
+        buf->buf[(buf->start + pos * DECODE_SAMPLE_RATIO + j) % QUEUE_MAX_SIZE];
+  }
+  if (num_ones >= DECODE_SAMPLE_RATIO_THRESHOLD)
+    return 1;
+  if (DECODE_SAMPLE_RATIO - num_ones >= DECODE_SAMPLE_RATIO_THRESHOLD)
+    return 0;
+  return -1;
+}
+
+int decode_packet(raw_packet_t *received_bits, size_t *decoded_size_byte,
+                  uint8_t decoded_data[PACKET_DATA_MAX_BYTE]) {
+  static queue_t buf;
+
+  // Append data to buffer
+  for (size_t i = 0; i < received_bits->size; i++) {
+    buf.buf[buf.end] = received_bits->bits[i];
+    buf.end = (buf.end + 1) % QUEUE_MAX_SIZE;
+  }
+
+  // TODO: profile and optimize
+  while (1) {
+    // Check if buffer has enough data
+    if (queue_size(&buf) < PACKET_MAX_LEN * DECODE_SAMPLE_RATIO)
+      return 0;
+
+    // Find start sequence
+    for (size_t i = 0; i < PACKET_START_LEN; i++) {
+      if (decode_bit_at_pos(&buf, i) != PACKET_START[i])
+        goto next;
+    }
+
+    // Find size (little-endian)
+    int size = 0;
+    for (size_t i = 0; i < PACKET_SIZE_LEN; i++) {
+      int bit = decode_bit_at_pos(&buf, PACKET_START_LEN + i);
+      if (bit == -1)
+        goto next;
+      size |= bit << i;
+    }
+    if (size == 0)
+      goto next;
+
+    // Parse data (little-endian)
+    int parity = 0;
+    for (int i = 0; i < size; i++) {
+      uint8_t byte = 0;
+      for (int j = 0; j < 8; j++) {
+        int bit = decode_bit_at_pos(&buf, PACKET_START_LEN + PACKET_SIZE_LEN +
+                                              i * 8 + j);
+        if (bit == -1)
+          goto next;
+        byte |= bit << j;
+        parity ^= bit;
+      }
+      decoded_data[i] = byte;
+    }
+    *decoded_size_byte = size;
+
+    // Check parity
+    if (parity !=
+        decode_bit_at_pos(&buf, PACKET_START_LEN + PACKET_SIZE_LEN + size * 8))
+      goto next;
+
+    // Find end sequence
+    for (size_t i = 0; i < PACKET_END_LEN; i++) {
+      if (decode_bit_at_pos(&buf, PACKET_START_LEN + PACKET_SIZE_LEN +
+                                      size * 8 + PACKET_PARITY_LEN + i) !=
+          PACKET_END[i])
+        goto next;
+    }
+
+    // Remove the packet from buffer
+    buf.start = (buf.start + (PACKET_START_LEN + PACKET_SIZE_LEN + size * 8 +
+                              PACKET_PARITY_LEN + PACKET_END_LEN) *
+                                 DECODE_SAMPLE_RATIO) %
+                QUEUE_MAX_SIZE;
+
+    return 1;
+
+  next:
+    buf.start++;
+  }
+}
---
+++ game/test-infrared.c
@@ -0,0 +1,89 @@
+#include "infrared.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void test_encode() {
+  uint8_t data[16] = {0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef,
+                      0xfe, 0xdc, 0xba, 0x98, 0x76, 0x54, 0x32, 0x10};
+  raw_packet_t packet = encode_packet(16, data);
+
+  size_t i = 0;
+
+  printf("[encode]\n");
+  printf("start: ");
+  for (; i < PACKET_START_LEN; i++) {
+    printf("%d%c", packet.bits[i], i == PACKET_START_LEN - 1 ? '\n' : ' ');
+  }
+
+  printf("size: ");
+  for (size_t j = 0; j < PACKET_SIZE_LEN; j++) {
+    printf("%d%c", packet.bits[i++], j == PACKET_SIZE_LEN - 1 ? '\n' : ' ');
+  }
+
+  printf("data:\n");
+  for (int j = 0; j < 16; j++) {
+    for (int k = 0; k < 8; k++) {
+      printf("%d ", packet.bits[i++]);
+    }
+    printf(" (should be 0x%02x)\n", data[j]);
+  }
+
+  printf("parity: %d\n", packet.bits[i++]);
+
+  printf("end: ");
+  for (size_t j = 0; j < PACKET_END_LEN; j++) {
+    printf("%d%c", packet.bits[i++], j == PACKET_END_LEN - 1 ? '\n' : ' ');
+  }
+}
+
+void test_decode() {
+  uint8_t _data[16] = {0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef,
+                       0xfe, 0xdc, 0xba, 0x98, 0x76, 0x54, 0x32, 0x10};
+  raw_packet_t _packet = encode_packet(16, _data);
+
+  // prepare received packet (upscaled by 4 times and simulate noise)
+  raw_packet_t packet;
+  for (size_t i = 0; i < _packet.size; i++) {
+    packet.bits[i * 4 + 0] = _packet.bits[i];
+    packet.bits[i * 4 + 1] = _packet.bits[i];
+    packet.bits[i * 4 + 2] = _packet.bits[i];
+    packet.bits[i * 4 + 3] = _packet.bits[i];
+    // randomly flip bits
+    int r = rand() % 5;
+    if (r != 4) {
+      packet.bits[i * 4 + r] = !packet.bits[i * 4 + r];
+    }
+  }
+  packet.size = _packet.size * 4;
+
+  size_t decoded_byte_size;
+  uint8_t decoded_data[32];
+  size_t batch_size = 5; // change me
+  size_t i = 0;
+  for (; i < 2 * PACKET_MAX_LEN * DECODE_SAMPLE_RATIO; i += batch_size) {
+    raw_packet_t batch;
+    batch.size = 0;
+    for (size_t j = 0; j < batch_size; j++) {
+      batch.bits[j] = (i + j < packet.size) ? packet.bits[i + j] : rand() % 2;
+      batch.size++;
+    }
+
+    if (decode_packet(&batch, &decoded_byte_size, decoded_data)) {
+      printf("[decode]\n");
+      printf("decoded size: %zu\n", decoded_byte_size);
+      printf("decoded data:\n");
+      for (size_t j = 0; j < decoded_byte_size; j++) {
+        printf("0x%02x ", decoded_data[j]);
+      }
+      printf("\n");
+    }
+  }
+}
+
+int main() {
+  test_encode();
+  puts("");
+  test_decode();
+
+  return 0;
+}
