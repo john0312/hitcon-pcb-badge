56ad0d9e116b801cc7cde2626dd028b8ee99285d
Chi-Feng Tsai <ktpss95112@gmail.com>
2024-05-04 17:12:06 +0800

    add LED display library

---
+++ game/display.c
@@ -0,0 +1,108 @@
+#include "display.h"
+#include <string.h>
+
+static uint8_t display_buf[DISPLAY_HEIGHT][DISPLAY_WIDTH];
+
+static int display_mode;
+static int
+    display_current_frame; // will be updated when display_get_frame is called
+
+// TODO: use union to save memory if we want to store other info for other modes
+struct {
+  uint8_t buf[DISPLAY_HEIGHT * DISPLAY_SCROLL_MAX_COLUMNS];
+  int first_frame;
+  int cols;
+  int speed;
+} display_scroll_info;
+
+void get_scroll_frame(uint8_t *buf, int frame) {
+  /**
+   * The content will scroll from right to left, and the first frame of the
+   * scrolling is an empty screen.
+   *
+   * In the following illustration:
+   * s = speed
+   * T = (DISPLAY_WIDTH + cols + 1) * speed
+   * (which is the period of the scrolling)
+   *
+   *                      +------------------------------------+
+   * scroll buffer        |                                    |
+   *                      +------------------------------------+
+   * frame = [0, s)  +---+
+   * display buffer  |   |
+   *                 +---+
+   * frame = [s, 2s)  +---+
+   * display buffer   |   |
+   *                  +---+
+   * frame = [2s, 3s)  +---+
+   * display buffer    |   |
+   *                   +---+
+   * ...
+   * frame = [T - 3s, T - 2s)                                 +---+
+   * display buffer                                           |   |
+   *                                                          +---+
+   * frame = [T - 2s, T - s)                                   +---+
+   * display buffer                                            |   |
+   *                                                           +---+
+   * frame = [T - s, T)                                         +---+
+   * display buffer                                             |   |
+   *                                                            +---+
+   */
+
+  int total_width = DISPLAY_WIDTH + display_scroll_info.cols + 1;
+  int period = total_width * display_scroll_info.speed;
+  int x_at_frame0 = -DISPLAY_WIDTH;
+  int current_x = x_at_frame0 + (frame - display_scroll_info.first_frame) %
+                                    period / display_scroll_info.speed;
+
+  for (int y = 0; y < DISPLAY_HEIGHT; y++) {
+    for (int x = 0; x < DISPLAY_WIDTH; x++) {
+      buf[y * DISPLAY_WIDTH + x] =
+          (0 <= current_x + x && current_x + x < display_scroll_info.cols)
+              ? display_scroll_info
+                    .buf[y * display_scroll_info.cols + current_x + x]
+              : 0;
+    }
+  }
+}
+
+void display_init() {
+  display_mode = DISPLAY_MODE_BLANK;
+  memset(display_buf, 0, sizeof(display_buf));
+}
+
+void display_get_frame(uint8_t *buf, int frame) {
+  switch (display_mode) {
+  case DISPLAY_MODE_BLANK:
+    memset(buf, 0, sizeof(display_buf));
+    break;
+
+  case DISPLAY_MODE_FIXED:
+    memcpy(buf, display_buf, sizeof(display_buf));
+    break;
+
+  case DISPLAY_MODE_SCROLL:
+    get_scroll_frame(buf, frame);
+    break;
+  }
+
+  display_current_frame = frame;
+}
+
+void display_set_mode_blank() {
+  display_mode = DISPLAY_MODE_BLANK;
+  memset(display_buf, 0, sizeof(display_buf));
+}
+
+void display_set_mode_fixed(uint8_t *buf) {
+  display_mode = DISPLAY_MODE_FIXED;
+  memcpy(display_buf, buf, sizeof(display_buf));
+}
+
+void display_set_mode_scroll(uint8_t *buf, int cols, int speed) {
+  display_mode = DISPLAY_MODE_SCROLL;
+  display_scroll_info.first_frame = display_current_frame;
+  display_scroll_info.cols = cols;
+  display_scroll_info.speed = speed;
+  memcpy(display_scroll_info.buf, buf, DISPLAY_HEIGHT * cols);
+}
---
+++ game/test-display.c
@@ -0,0 +1,65 @@
+#include "display.h"
+#include <stdio.h>
+#include <unistd.h>
+
+void print_buf(int frame, uint8_t *buf) {
+  printf("frame %d\n", frame);
+  for (int y = 0; y < DISPLAY_HEIGHT; y++) {
+    for (int x = 0; x < DISPLAY_WIDTH; x++) {
+      printf("%c%c",
+             buf[y * DISPLAY_HEIGHT + x] == 0
+                 ? '.'
+                 : '0' + buf[y * DISPLAY_HEIGHT + x],
+             x == DISPLAY_WIDTH - 1 ? '\n' : ' ');
+    }
+  }
+}
+
+int main() {
+  display_init();
+  uint8_t buf[DISPLAY_HEIGHT * DISPLAY_WIDTH];
+  int frame = 0;
+
+  puts("[blank]");
+  display_set_mode_blank();
+  display_get_frame(buf, frame++);
+  print_buf(frame, buf);
+  display_get_frame(buf, frame++);
+  print_buf(frame, buf);
+  display_get_frame(buf, frame++);
+  print_buf(frame, buf);
+  puts("");
+
+  puts("[fixed]");
+  uint8_t buf_fixed[DISPLAY_HEIGHT * DISPLAY_WIDTH] = {
+      1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1,
+      0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0,
+      0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1,
+  };
+  display_set_mode_fixed(buf_fixed);
+  display_get_frame(buf, frame++);
+  print_buf(frame, buf);
+  display_get_frame(buf, frame++);
+  print_buf(frame, buf);
+  display_get_frame(buf, frame++);
+  print_buf(frame, buf);
+  puts("");
+
+  puts("[scroll]");
+  uint8_t buf_scroll[] = {
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+      1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0,
+      0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0,
+      0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0,
+      0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0,
+  };
+  int speed = 3;
+  display_set_mode_scroll(buf_scroll, sizeof(buf_scroll) / 8, speed);
+  for (int i = 0; i < 100; i++) {
+    display_get_frame(buf, frame++);
+    print_buf(frame, buf);
+    usleep(1000000 / 10 / speed);
+  }
+
+  return 0;
+}
