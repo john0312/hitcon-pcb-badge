0cc8e63966fb3fad8ee16dbd136113fd7a3de392
John L Chen <zuan@chromium.org>
2024-05-19 23:33:04 +0800

    Add MenuTree definition

---
+++ fw/Core/Hitcon/Logic/MenuTree.cc
@@ -0,0 +1,17 @@
+#include <Logic/MenuTree.h>
+
+namespace hitcon {
+
+MenuTree::MenuTree() {
+}
+
+void MenuTree::OnButton(button_t button) {
+}
+
+void MenuTree::OnCrossBoardPacket(uint8_t* packet) {
+}
+
+void MenuTree::OnUsbInsertion() {
+}
+
+}
