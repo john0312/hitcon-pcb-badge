23070beded52376bc3d855a217a0c92c257fefd5
John L Chen <zuan@chromium.org>
2024-06-02 23:12:58 +0800

    Add definition for NvStorage

---
+++ fw/Core/Hitcon/Service/NvStorage.cc
@@ -0,0 +1,16 @@
+#include <Service/NvStorage.h>
+
+namespace hitcon {
+
+NvStorage::NvStorage() {
+}
+
+void NvStorage::Init() {
+  // TODO
+}
+
+void NvStorage::ForceFlush(callback_t on_done, void* callback_arg1) {
+  // TODO
+}
+
+}  // namespace hitcon
