d93eb6dbe4b34a45d8a0812b2b2eb35133912762
Chi-Feng Tsai <ktpss95112@gmail.com>
2024-06-02 21:47:57 +0800

    refactor button

--- fw/Core/Hitcon/Logic/BadgeController.cc
+++ fw/Core/Hitcon/Logic/BadgeController.cc
@@ -14,10 +14,22 @@ void BadgeController::OnButton(void *arg1, void *arg2) {
   BadgeController *this_ = static_cast<BadgeController *>(arg1);
   button_t button = static_cast<button_t>(reinterpret_cast<uintptr_t>(arg2));
 
-  // TODO: if button means change app, change app (current_app.OnExit(),
-  // new_app.OnEntry())
+  switch (button) {
+    case BUTTON_MODE:
+      // TODO: if button means change app, change app (current_app.OnExit(),
+      // new_app.OnEntry())
+      break;
 
-  // TODO: else, forward the button to the current app
+    case BUTTON_BRIGHTNESS:
+    case BUTTON_LONG_BRIGHTNESS:
+      // TODO: change brightness
+      break;
+
+    default:
+      // forward the button to the current app
+      this_->current_app->OnButton(button);
+      break;
+  }
 }
 
 } // namespace hitcon
