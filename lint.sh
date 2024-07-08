2e0c13b8995d869f4846e128c6bffab00d55a4a5
Alx-Lai <alexabc722@gmail.com>
2024-07-08 22:21:11 +0800

    find doesn't return array

--- lint.sh
+++ lint.sh
@@ -21,18 +21,19 @@ TARGET_FILES=$(find $TARGET_DIR -type f -name "*.cc" -o -name "*.cpp" -o -name "
 
 if $auto_fmt; then 
   echo "Auto format"
-  for file in "${TARGET_FILES[@]}"; do
-    echo "Formatting $file"
-    clang-format -i $file &> /dev/null
-  done
+  while read file; do
+      echo "Formatting $file"
+      clang-format -i $file &> /dev/null
+  done <<< "$TARGET_FILES"
 else
   echo "Check format"
-  for file in "${TARGET_FILES[@]}"; do
-    clang-format --dry-run --Werror $file &> /dev/null
+  while read file; do
+    echo $file
+    clang-format --dry-run --Werror $file
     ret=$?
     if [[ $ret -ne 0 ]]; then
       echo "Fail on $file"
       exit $ret
     fi
-  done
+  done <<< "$TARGET_FILES"
 fi
