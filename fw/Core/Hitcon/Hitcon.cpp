794aa39053bdc638a55948d33df6c6107cb6c687
Lance798 <hz946128@gmail.com>
2024-07-14 03:05:16 +0800

    Fix IR HW Test

--- fw/Core/Hitcon/Hitcon.cpp
+++ fw/Core/Hitcon/Hitcon.cpp
@@ -16,7 +16,7 @@ using namespace hitcon::service::sched;
 uint8_t hidata[]{0xAA, 0xb, 0xc, 0xd, 0xe, 0xf};
 void cb(void* unused1, void* unused2) {
   size_t len = 6;
-  hitcon::ir::irService.SendBuffer(hidata, len, true);
+  // hitcon::ir::irService.SendBuffer(hidata, len, true);
 }
 
 Task InitTask(1, (task_callback_t)&cb, nullptr);
@@ -24,7 +24,7 @@ Task InitTask(1, (task_callback_t)&cb, nullptr);
 void test(void* unused1, void* unused2) {}
 
 void hitcon_run() {
-  hitcon::ir::irService.SetOnBufferReceived(test, nullptr);
+  // hitcon::ir::irService.SetOnBufferReceived(test, nullptr);
   hitcon::ir::irService.Init();
   scheduler.Queue(&InitTask, nullptr);
   scheduler.Run();
