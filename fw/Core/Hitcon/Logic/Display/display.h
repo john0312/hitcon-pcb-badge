4bc257b349a18c644100a2898c8c6bacc2a3d5ef
Lance798 <hz946128@gmail.com>
2024-07-14 08:51:05 +0800

    Fix DisplayService::PopulateFrames

--- fw/Core/Hitcon/Logic/Display/display.h
+++ fw/Core/Hitcon/Logic/Display/display.h
@@ -1,8 +1,8 @@
 #ifndef DISPLAY_H
 #define DISPLAY_H
 
-#include <stdint.h>
 #include <Logic/Display/font.h>
+#include <stdint.h>
 
 /**
  * The underlying buffer is a 2D array of uint8_t like this:
@@ -13,8 +13,8 @@
  * buf[DISPLAY_HEIGHT - 1][0] ... buf[DISPLAY_HEIGHT - 1][DISPLAY_WIDTH - 1]
  */
 
-#define DISPLAY_HEIGHT 8 // fixed
-#define DISPLAY_WIDTH 16 // adjustable, maybe 8, 12, 16
+#define DISPLAY_HEIGHT 8  // fixed
+#define DISPLAY_WIDTH 16  // adjustable, maybe 8, 12, 16
 
 #define DISPLAY_MODE_BLANK 0
 #define DISPLAY_MODE_FIXED 1
@@ -27,22 +27,24 @@
 // DISPLAY_HEIGHT = 8 fixed -> 1 column/byte to reduce mem usage
 using display_buf_t = uint8_t;
 
-#define DISPLAY_SET(x, bit) do { \
-    (x) |= (1 << (bit)); \
-} while (0)
+#define DISPLAY_SET(x, bit) \
+  do {                      \
+    (x) |= (1 << (bit));    \
+  } while (0)
 
-#define DISPLAY_UNSET(x, bit) do { \
-    (x) &= (1<<DISPLAY_HEIGHT) - 1 - (1 << (bit)); \
-} while (0)
+#define DISPLAY_UNSET(x, bit)                        \
+  do {                                               \
+    (x) &= (1 << DISPLAY_HEIGHT) - 1 - (1 << (bit)); \
+  } while (0)
 
-#define DISPLAY_render_char(buf, ch, x, y, max_x, max_y)                  \
+#define DISPLAY_render_char(buf, ch, x, y, max_x, max_y)          \
   do {                                                            \
     for (int _y = 0; _y < CHAR_HEIGHT && y + _y < max_y; ++_y) {  \
       for (int _x = 0; _x < CHAR_WIDTH && x + _x < max_x; ++_x) { \
         if (rasterize_char_5x8(ch, _y, _x))                       \
-            DISPLAY_SET(buf[x + _x], y + _y);                     \
+          DISPLAY_SET(buf[x + _x], y + _y);                       \
         else                                                      \
-            DISPLAY_UNSET(buf[x + _x], y + _y);                   \
+          DISPLAY_UNSET(buf[x + _x], y + _y);                     \
       }                                                           \
     }                                                             \
   } while (0)
