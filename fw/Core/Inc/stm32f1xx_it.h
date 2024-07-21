ef0b03e08b19e479d0bc56704aafa17682e06386
Lance798 <hz946128@gmail.com>
2024-07-21 21:32:05 +0800

    Change project MCU F103C8 -> F103CB
    - Change project MCU F103C8 -> F103CB
    - Add Flash global interrupt

--- fw/Core/Inc/stm32f1xx_it.h
+++ fw/Core/Inc/stm32f1xx_it.h
@@ -55,6 +55,7 @@ void SVC_Handler(void);
 void DebugMon_Handler(void);
 void PendSV_Handler(void);
 void SysTick_Handler(void);
+void FLASH_IRQHandler(void);
 void DMA1_Channel1_IRQHandler(void);
 void DMA1_Channel2_IRQHandler(void);
 void DMA1_Channel4_IRQHandler(void);
