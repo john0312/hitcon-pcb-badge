09005f2ba987959e1300ccdacefc2bfa291517be
John L Chen <zuan@chromium.org>
2024-07-22 03:34:53 +0800

    init OnBufferReceived

--- fw/Core/Hitcon/Logic/IrLogic.cc
+++ fw/Core/Hitcon/Logic/IrLogic.cc
@@ -1,7 +1,9 @@
+#include "IrLogic.h"
+
 #include <Logic/IrLogic.h>
 #include <Service/IrService.h>
 
-#include <type_traits>
+#include <cstdint>
 
 namespace hitcon {
 namespace ir {
@@ -15,28 +17,93 @@ void IrLogic::Init() {
   irService.SetOnBufferReceived((callback_t)&IrLogic::OnBufferReceived, this);
 }
 
+size_t packet_buf{0};
+uint8_t packet_state{0};
+
+enum PACKET_STATE {
+  STATE_START = 0,
+  STATE_SIZE = 1,
+  STATE_DATA = 2,
+};
+
+enum BIT_STATE {
+  BIT_OFF = 0,
+  BIT_ON = 1,
+  BIT_INVALID = 2,
+};
+
+static uint8_t decode_bit(uint8_t x) {
+  uint8_t cnt = 0;
+  while (x > 0) {
+    cnt++;
+    x -= x & (-x);
+  }
+  switch (cnt) {
+    case 0:
+    case 1:
+      return BIT_OFF;
+    case 3:
+    case 4:
+      return BIT_ON;
+    case 2:
+    default:
+      return BIT_INVALID;
+  }
+}
+
 void IrLogic::OnBufferReceived(uint8_t *buffer) {
   // receive buffer -> check if packet_end -> call callback
-  std::remove_const<decltype(PACKET_END_)>::type check{0};
-  for (size_t i = 0; i < PACKET_END_LEN - 1; i++) {
-    check <<= 1;
-    check |= buffer[i];
-    packet_queue.push(buffer[i]);
-  }
+  // buffer starts from LSB
 
-  // TODO: check if the buffer size is IR_SERVICE_BUFFER_SIZE
-  for (size_t i = PACKET_END_LEN - 1; i < IR_SERVICE_BUFFER_SIZE; i++) {
-    check <<= 1;
-    check |= buffer[i];
-    check &= PACKET_END_MASK;
-    packet_queue.push(buffer[i]);
-
-    if (check == PACKET_END_) {
-      // get a packet
-      // TODO: make a packet from queue_t
-      // packet cannot be local var
-      // IrPacket packet;
-      // this->callback(this->callback_arg, packet);
+  // Here is a DOS feature that if someone send a packet_header
+  // then it can cause decode + receive fail
+  static IrPacket packet;
+  static uint8_t bit{0};
+  for (size_t i = 0; i < IR_SERVICE_BUFFER_SIZE; i++) {
+    for (uint8_t j = 0; j < 8; j++) {
+      uint8_t is_on = (buffer[i] & (1 << j)) ? 1 : 0;
+      switch (packet_state) {
+        case STATE_START:
+          packet_buf <<= 1;
+          packet_buf |= is_on;
+          packet_buf &= IR_PACKET_HEADER_MASK;
+          if (packet_buf == IR_PACKET_HEADER_PACKED) {
+            packet_state = STATE_SIZE;
+            packet_buf = 0;
+          }
+          break;
+        case STATE_SIZE:
+          // here, denote packet_buf as n of consumed size bit
+          packet_buf++;
+          bit <<= 1;
+          bit |= is_on;
+          if ((packet_buf & 3) == 0) {
+            packet.size_ = ((packet.size_ << 1) | decode_bit(bit));
+            bit = 0;
+          }
+          // end of size section (decode ratio * 1 byte)
+          if (packet_buf == 4 * 8) packet_state = STATE_DATA;
+          break;
+        case STATE_DATA:
+          // here, denote packet_buf as n of consumed data bit
+          packet_buf++;
+          bit <<= 1;
+          bit |= is_on;
+          if ((packet_buf & 3) == 0) {
+            if (decode_bit(bit) == BIT_INVALID) {
+              packet_state = STATE_START;
+              return;
+            }
+            packet.data_[packet_buf >> 2] = decode_bit(bit);
+          }
+          if ((packet_buf >> 2) == packet.size_) {
+            // packet_end
+            callback(callback_arg, reinterpret_cast<void *>(&packet));
+          }
+          break;
+        default:
+          break;
+      }
     }
   }
 }
