2d2ff5a357040faaf872c25e6b5af55678a15bc2
Lisa Wang <wyihan@google.com>
2024-07-25 23:37:44 +0800

    Basic functions (Successfully Build)

--- fw/Core/Hitcon/Logic/IrController.cc
+++ fw/Core/Hitcon/Logic/IrController.cc
@@ -1,9 +1,11 @@
-#include "IrController.h"
-#include "game.h"
+#include <Logic/IrController.h>
+#include <Logic/game.h>
+#include <Service/IrService.h>
 #include <stdlib.h>
 #include <time.h>
 
 using namespace hitcon::service::sched;
+
 namespace hitcon {
 namespace ir {
 
@@ -12,39 +14,37 @@ constexpr int IrAllowBroadcastCnt = 5;
 
 IrController::IrController()
     : routine_task(950, (callback_t)&IrController::RoutineTask, this, 1000),
-      recv_lock(true), send_lock(true);
-    {
-      srand(time(NULL));
-      Init();
-    }
-}
+      broadcast_task(800, (callback_t)&IrController::BroadcastIr, this),
+      send2game_task(800, (callback_t)&IrController::Send2Game, this),
+      send_lock(true), recv_lock(true) {}
 
-void IrController::Send2Game() {
-  game_accept_data(callback_col, callback_data);
+void IrController::Send2Game(void* arg) {
+  GamePacket* game = reinterpret_cast<GamePacket*>(arg);
+  game_accept_data(game->col, game->data);
   send_lock = true;
 }
 
 void IrController::Init() {
   irLogic.SetOnPacketReceived((callback_t)&IrController::OnPacketReceived,
                               this);
-  // Initialize v[0]
+  // TODO: Remove the srand and time
+  srand(time(NULL));
 }
-
 
 void IrController::OnPacketReceived(void* arg) {
   IrPacket* packet = reinterpret_cast<IrPacket*>(arg);
-  callback_col = packet->size;
-  callback_data = packet->data;
+  IrData* data = reinterpret_cast<IrData*>(packet->data_);
 
-  if (send_lock) {
-    send_lock = false;
-    task(800, (task_callback_t)&IrController::Send2Game, (void*)this, 1000);
+  // Game
+  if (data->packet_type == 0) {
+    if (send_lock) {
+      send_lock = false;
+      scheduler.Queue(&send2game_task, &data->game);
+    }
   }
 }
 
-int IrController::prob_f(int lf) {
-  return v[0] * lf * lf + v[1] * lf + v[2];
-}
+int IrController::prob_f(int lf) { return v[0] * lf * lf + v[1] * lf + v[2]; }
 
 void IrController::RoutineTask(void* unused) {
   // Update parameters from load factor.
@@ -54,12 +54,15 @@ void IrController::RoutineTask(void* unused) {
   int rand_num = rand() % RAND_MAX;
   if (rand_num > prob_f(lf) && send_lock) {
     send_lock = false;
-    task(800, (callback_t)&IrController::BroadcastIr, (void*)this, 1000);
+    scheduler.Queue(&broadcast_task, nullptr);
   }
 }
 
-void IrController::BroadcastIr() {
+void IrController::BroadcastIr(void* unused) {
   int type = rand() % IrAllowBroadcastCnt;
+  for (int i = 0; i < GAME_DATA_SIZE; ++i) {
+    // Get data and send to packet
+  }
   send_lock = true;
 }
 
