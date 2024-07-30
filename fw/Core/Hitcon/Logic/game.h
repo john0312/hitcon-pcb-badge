bdf9ebb12da6eb4d6ab6a73eb486711e76562ab7
Alx-Lai <alexabc722@gmail.com>
2024-07-30 21:52:11 +0800

    implement basic game func

--- fw/Core/Hitcon/Logic/game.h
+++ fw/Core/Hitcon/Logic/game.h
@@ -30,6 +30,34 @@ uint8_t* get_data_cell(int col, int row);
 // Get a random data cell for sending over IR. The data is written to out_data, while the column is written to out_col.
 bool get_random_cell_data_for_ir_transmission(uint8_t* out_data, int* out_col);
 
+struct GameQueue {
+  std::pair<int, grid_cell_t> datas[kQueueSize];
+  // [start, end)
+  uint8_t queue_start = 0;
+  uint8_t queue_end = 0;
+  uint8_t size() {
+    return (queue_end - queue_start) > 0 ?
+    (queue_end - queue_start) :
+    (queue_end - queue_start + kQueueSize);
+  }
+  bool push(int col, grid_cell_t data) {
+    if (size() == 0) return false;
+    data[queue_end++] = std::make_pair(col, data);
+    if(queue_end == kQueueSize)
+      queue_end = 0;
+    return true;
+  }
+  std::pair<int, grid_cell_t> pop() {
+    if (size() == 0) return (grid_cell_t)0;
+    std::pair<int, grid_cell_t> ret = data[queue_start--];
+    if (queue_start < 0)
+      queue_start += kQueueSize;
+    return ret;
+  }
+};
+
+extern GameQueue game_queue;
+
 }  // namespace game
 }  // namespace hitcon
 
