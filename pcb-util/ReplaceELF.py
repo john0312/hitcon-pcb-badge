faef3970867ce84b777781b3efd30215c3de794b
Yen Lin <a870123tw@gmail.com>
2024-08-01 23:44:42 +0800

    Make ReplaceELF capable of replacing to randomly generated arrays

--- pcb-util/ReplaceELF.py
+++ pcb-util/ReplaceELF.py
@@ -1,5 +1,6 @@
 import shutil
 from elftools.elf.elffile import ELFFile
+import numpy as np
 
 
 def duplicate_elf_file(original_file_path, new_file_path):
@@ -79,40 +80,68 @@ def replace_array_in_elf(elf_file_path, search_array, replace_array):
         f.write(bytes(replace_array))
         print(f"Replace array at offset: {hex(array_offset)}")
 
-# Example usage
-original_elf_file_path = 'fw.elf'  # Original ELF file path
-duplicated_elf_file_path = 'fwMOD.elf'  # New duplicated ELF file path
-
-search_array = [
-    0xf1, 0xca, 0x4e, 0xa0, 0x48, 0x2f, 0x27, 0x4d,
-    0x3d, 0xc2, 0x9c, 0x8c, 0xec, 0x36, 0x83, 0x49
-]  # Array to search for
-replace_array = [
-    0x13, 0xac, 0x76, 0xfc, 0x1a, 0xa7, 0x0f, 0x92,
-    0x05, 0x31, 0x1d, 0xa6, 0x28, 0x4c, 0x8e, 0x94
-]  # New array to replace with
-elf_file_path = 'fwMOD.elf'  # ELF file path
-
-# Duplicate the ELF file
-duplicate_elf_file(original_elf_file_path, duplicated_elf_file_path)
-
-# Find and Replace the array
-array_offset, chunk = find_array_in_elf(elf_file_path, search_array)
-if array_offset is not None:
-    print(f"Array found at offset: {hex(array_offset)}")
-    print_hex_around_target_array(elf_file_path, array_offset, search_array)
-    
-    # Replace the array
-    replace_array_in_elf(elf_file_path, search_array, replace_array)
-
-    # Find the replaced array again to verify the replacement
-    array_offset, chunk = find_array_in_elf(elf_file_path, replace_array)
+# Find and Replace the array of PerBoardRandom
+def search_and_reaplce_array(elf_file_path, search_array, replace_array):
+    array_offset, chunk = find_array_in_elf(elf_file_path, search_array)
     if array_offset is not None:
-        print(f"Array replaced at offset: {hex(array_offset)}")
-        print_hex_around_target_array(elf_file_path, array_offset, replace_array)
+        print(f"Array found at offset: {hex(array_offset)}")
+        print_hex_around_target_array(elf_file_path, array_offset, search_array)
+        
+        # Replace the array
+        replace_array_in_elf(elf_file_path, search_array, replace_array)
+
+        # Find the replaced array again to verify the replacement
+        array_offset, chunk = find_array_in_elf(elf_file_path, replace_array)
+        if array_offset is not None:
+            print(f"Array replaced at offset: {hex(array_offset)}")
+            print_hex_around_target_array(elf_file_path, array_offset, replace_array)
+        else:
+            print("Array failed to be replaced in the ELF file.")
     else:
-        print("Array failed to be replaced in the ELF file.")
+        print("Array not found in the ELF file.")
 
-else:
-    print("Array not found in the ELF file.")
+def print_array_in_hex(array):
+    hex_array = [f'0x{value:02x}' for value in array]
+    print("Generated random uint8 array in HEX:", hex_array)
+    
 
+if __name__ == "__main__":
+
+    original_elf_path = 'fw.elf'  # Original ELF file path
+    MOD_elf_path = 'fwMOD.elf'  # New duplicated ELF file path
+
+    # Configure the array to search in ELF
+    search_array_PerBoardRandom = [
+            0xf1, 0xca, 0x4e, 0xa0, 0x48, 0x2f, 0x27, 0x4d,
+            0x3d, 0xc2, 0x9c, 0x8c, 0xec, 0x36, 0x83, 0x49
+        ]  # Array to search for PerBoardRandom
+        
+    search_array_PerBoardSecret = [
+            0x13, 0xac, 0x76, 0xfc, 0x1a, 0xa7, 0x0f, 0x92,
+            0x05, 0x31, 0x1d, 0xa6, 0x28, 0x4c, 0x8e, 0x94
+        ]  # Array to search for PerBoardSecret
+    
+    # Generate Random array
+    replace_array_PerBoardRandom = np.random.randint(0, 256, size=16, dtype=np.uint8)
+    replace_array_PerBoardSecret = np.random.randint(0, 256, size=16, dtype=np.uint8)
+    
+    # Print random array
+    print_array_in_hex(replace_array_PerBoardRandom)
+    print_array_in_hex(replace_array_PerBoardSecret)
+    
+    # TODO: Record PerBoardSecret to Cloud
+    
+    print("\n TODO: Upload PerBoardSecret to Cloud \n")
+
+    # Duplicate the ELF file
+    duplicate_elf_file(original_elf_path, MOD_elf_path)
+
+    print("\n Operating fwMOD.elf...... \n")
+
+    # Search and replace PerBoardRandom array
+    search_and_reaplce_array(MOD_elf_path, search_array_PerBoardRandom, replace_array_PerBoardRandom)
+    
+    # Search and replace PerBoardSecret array
+    search_and_reaplce_array(MOD_elf_path, search_array_PerBoardSecret, replace_array_PerBoardSecret)
+    
+    print("\n Operation Done \n")    
