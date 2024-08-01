5b12617eddce37b5f74a48947a9a8843be4d261b
John L Chen <zuan@chromium.org>
2024-08-02 00:31:13 +0800

    Implement Routine() in GameLogic

--- fw/Core/Hitcon/Logic/GameLogic.h
+++ fw/Core/Hitcon/Logic/GameLogic.h
@@ -5,12 +5,18 @@
 #include <stdint.h>
 #include <utility>
 
+#include <Service/Sched/Scheduler.h>
 #include <Logic/GameParam.h>
 #include <Logic/keccak.h>
 #include <Util/CircularQueue.h>
 
 namespace hitcon {
 namespace game {
+
+// Implements the natural log ln() with approximate polynomial.
+// The output is in Q9.22 fixed point number, while the input is in
+// uint32_t integer format.
+int32_t q22_ln(uint32_t) ;
 
 class GameLogic {
  public:
@@ -35,6 +41,10 @@ class GameLogic {
   game_cache_t cache_;
 
   enum RoutineState {
+    // Sets the cell data to be random.
+    // Each run of Routine() in this state populates one column, and uses
+    // in_progress_col_.
+    RANDOM_INIT_CELLS,
     // In this phase, Routine iterates through every cell in storage_ and
     // populates the corresponding cell_score_cache. After every cell in
     // cell_score_cache is populated, it'll move onto the next phase.
@@ -83,6 +93,8 @@ class GameLogic {
   // The contet for the current in-progress SHA3 256 computation.
   sha3_context routine_sha3_256_context_;
 
+  const uint8_t* routine_sha3_hash_out_;
+
   // Which column are we populating in POPULATING_CACHE_CELLS state?
   int populating_cache_col_;
   // Which row are we populating in POPULATING_CACHE_CELLS state?
@@ -95,9 +107,12 @@ class GameLogic {
   int in_progress_col_;
   grid_cell_t in_progress_data_;
 
+  // Computed in progress score for processing of AcceptData.
+  int in_progress_cell_score_;
+
   CircularQueue<std::pair<int, grid_cell_t>, kQueueSize> queue_;
 
-  void InitGameStorage();
+  hitcon::service::sched::PeriodicTask routine_task;
 
   // A simple helper function that sets out to the column prefix for the
   // given column. It'll be set to the value of "HITCON" and the last 2 byte
@@ -107,6 +122,27 @@ class GameLogic {
   // A simple routine function that attempts to populate the cache and handle
   // any items in the AcceptData queue.
   void Routine();
+
+  // Iterate through the sub routine enums one step per call.
+  // If we just finished the last step in the sub routine, the we'll return true
+  // and out_score is set to the score of the give col/cell_data.
+  // Otherwise, false is returned and we should call StepSubRoutine() again in
+  // the next Routine invocation.
+  bool StepSubRoutine(int col, uint8_t* cell_data, int* out_score);
+
+  // Computes the number of prefix 0 bit in the given bin_hash, which is a hash
+  // of sha3 256 bit variant.
+  int ComputePrefixZero(const uint8_t* bin_hash);
+
+  // Computes the columns's score from all the cell in the column.
+  // Updates the cache after the computation.
+  void ComputeColumnScore(int col);
+
+  // Computes the final score from the column score.
+  // Updates the cache after the computation.
+  void ComputeFinalScore();
+
+  void RandomlySetGridCellValue(int row, int col);
 };
 
 extern GameLogic gameLogic;
