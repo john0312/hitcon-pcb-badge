cedbb15a37ee289f2ea63955be628068a045d87f
Chi-Feng Tsai <ktpss95112@gmail.com>
2024-08-05 03:40:34 +0800

    add tetris game and test

---
+++ fw/Core/Hitcon/App/TetrisGame.cc
@@ -0,0 +1,164 @@
+#include "TetrisGame.h"
+
+namespace hitcon {
+namespace tetris {
+
+TetrisGame::TetrisGame(unsigned (*rand)(void)) : rand(rand) {
+  generate_new_tetromino();
+}
+
+void TetrisGame::clear_full_line() {
+  int from = BOARD_HEIGHT - 1;
+  int to = BOARD_HEIGHT - 1;
+  for (; from >= 0; from--) {
+    if (board[from] != 0b11111111u) {
+      board[to] = board[from];
+      if (to != from) {
+        board[from] = 0;
+      }
+      to--;
+    }
+  }
+  for (; to >= 0; to--) {
+    board[to] = 0;
+  }
+}
+
+// If success, return true and update current_x, current_y, current_rotation.
+bool TetrisGame::rotate_tetromino() {
+  int new_rotation = (current_rotation + 1) % 4;
+  const auto &t_old = TETROMINO[current_tetromino][current_rotation];
+  int new_x = current_x + t_old.rotate_dx;
+  int new_y = current_y + t_old.rotate_dy;
+  if (!place_tetromino(new_x, new_y, new_rotation)) {
+    return false;
+  }
+  return true;
+}
+
+// If success, return true and update current_x, current_y, current_rotation.
+bool TetrisGame::place_tetromino(int x, int y) {
+  return place_tetromino(x, y, current_rotation);
+}
+
+// If success, return true and update current_x, current_y, current_rotation.
+bool TetrisGame::place_tetromino(int x, int y, int rotation) {
+  const auto &t = TETROMINO[current_tetromino][rotation];
+  // check if the tetromino is out of the board
+  if (!(0 <= x && x + t.width <= BOARD_WIDTH)) {
+    return false;
+  }
+  if (!(0 <= y && y + t.height <= BOARD_HEIGHT)) {
+    return false;
+  }
+  // check if the position is occupied
+  for (int i = 0; i < t.height; i++) {
+    if (!!(board[y + i] & (t.data[i] << x))) {
+      return false;
+    }
+  }
+  // place the tetromino
+  for (int i = 0; i < t.height; i++) {
+    board[y + i] |= (t.data[i] << x);
+  }
+  current_x = x;
+  current_y = y;
+  current_rotation = rotation;
+  return true;
+}
+
+void TetrisGame::unplace_tetromino(int x, int y) {
+  const auto &t = TETROMINO[current_tetromino][current_rotation];
+  for (int i = 0; i < t.height; i++) {
+    board[y + i] &= ~(t.data[i] << x);
+  }
+}
+
+// If success, update current_y.
+// If fail, determine if the game is over or generate a new tetromino.
+void TetrisGame::fall_down_tetromino() {
+  unplace_tetromino(current_x, current_y);
+  if (place_tetromino(current_x, current_y + 1)) {
+    return;
+  }
+
+  // if fail, check if game is over
+  if (current_y == 0) {
+    game_over();
+    return;
+  }
+
+  // fix the tetromino at current position
+  place_tetromino(current_x, current_y);
+  clear_full_line();
+
+  // generate a new tetromino
+  if (!generate_new_tetromino()) {
+    game_over();
+  }
+}
+
+bool TetrisGame::generate_new_tetromino() {
+  current_tetromino = rand() % TETROMINO_COUNT;
+  const auto &t = TETROMINO[current_tetromino][0];
+  current_x = BOARD_WIDTH / 2 - t.width / 2;
+  current_y = 0;
+  current_rotation = 0;
+  return place_tetromino(current_x, current_y);
+}
+
+void TetrisGame::game_fall_down_tetromino() {
+  if (state == GAME_STATE_PLAYING) {
+    fall_down_tetromino();
+  }
+}
+
+void TetrisGame::game_on_input(TetrisDirection direction) {
+  if (state == GAME_STATE_PLAYING) {
+    switch (direction) {
+      case DIRECTION_LEFT:
+        unplace_tetromino(current_x, current_y);
+        if (!place_tetromino(current_x - 1, current_y)) {
+          place_tetromino(current_x, current_y);
+        }
+        break;
+
+      case DIRECTION_RIGHT:
+        unplace_tetromino(current_x, current_y);
+        if (!place_tetromino(current_x + 1, current_y)) {
+          place_tetromino(current_x, current_y);
+        }
+        break;
+
+      case DIRECTION_DOWN:
+        fall_down_tetromino();
+        break;
+
+      case DIRECTION_UP:
+        unplace_tetromino(current_x, current_y);
+        if (!rotate_tetromino()) {
+          place_tetromino(current_x, current_y);
+        }
+        break;
+
+      default:
+        break;
+    }
+  }
+}
+
+inline uint8_t reverse_bit(uint8_t x) {
+  x = ((x & 0b10101010u) >> 1) | ((x & 0b01010101u) << 1);
+  x = ((x & 0b11001100u) >> 2) | ((x & 0b00110011u) << 2);
+  x = ((x & 0b11110000u) >> 4) | ((x & 0b00001111u) << 4);
+  return x;
+}
+
+void TetrisGame::game_draw_to_display(display_buf_t *buf) {
+  for (int y = 0; y < BOARD_HEIGHT; y++) {
+    buf[y] = reverse_bit(board[y]);
+  }
+}
+
+}  // namespace tetris
+}  // namespace hitcon
---
+++ fw/Core/Hitcon/App/TetrisGame.h
@@ -0,0 +1,141 @@
+#ifndef TETRIS_GAME_H
+#define TETRIS_GAME_H
+
+#include <Logic/Display/display.h>
+
+namespace hitcon {
+namespace tetris {
+
+constexpr unsigned FALL_PERIOD = 1000;     // ms
+constexpr unsigned UPDATE_INTERVAL = 200;  // ms
+constexpr unsigned UPDATE_PRIORITY = 960;
+
+constexpr int BOARD_WIDTH = 8;
+constexpr int BOARD_HEIGHT = 16;
+constexpr int TETROMINO_COUNT = 7;
+static_assert(BOARD_WIDTH == DISPLAY_HEIGHT,
+              "BOARD_WIDTH must be equal to DISPLAY_HEIGHT");
+static_assert(BOARD_HEIGHT == DISPLAY_WIDTH,
+              "BOARD_HEIGHT must be equal to DISPLAY_WIDTH");
+
+enum TetrisGameState {
+  GAME_STATE_PLAYING,
+  GAME_STATE_GAME_OVER,
+};
+
+enum TetrisDirection {
+  DIRECTION_UP,
+  DIRECTION_LEFT,
+  DIRECTION_RIGHT,
+  DIRECTION_DOWN,
+};
+
+// clang-format off
+/**
+ * LSB <--> MSB
+ *
+ * I XXXX  J XXX   L XXX   O XX
+ *           ..X     X..     XX
+ *
+ * S .XX   Z XX.   T XXX
+ *   XX.     .XX     .X.
+ */
+const struct tetromino_t {
+  int width;
+  int height;
+  // The rotated tetromino should be placed at (x + rotate_dx, y + rotate_dy), i.e. TETROMINO[0][1]
+  // should be placed at (x + TETROMINO[0][0].rotate_dx, y + TETROMINO[0][0].rotate_dy).
+  int rotate_dx;
+  int rotate_dy;
+  uint8_t data[4];
+} TETROMINO[TETROMINO_COUNT][4] = {
+  { // I
+    {4, 1, 1, -1, {0b1111}},
+    {1, 4, -2, 1, {0b1, 0b1, 0b1, 0b1}},
+    {4, 1, 2, -2, {0b1111}},
+    {1, 4, -1, 2, {0b1, 0b1, 0b1, 0b1}},
+  },
+  { // J
+    {3, 2, 0, -1, {0b111, 0b100}},
+    {2, 3, 0, 0, {0b10, 0b10, 0b11}},
+    {3, 2, 1, 0, {0b001, 0b111}},
+    {2, 3, -1, 1, {0b11, 0b1, 0b1}},
+  },
+  { // L
+    {3, 2, 0, -1, {0b111, 0b1}},
+    {2, 3, 0, 0, {0b11, 0b10, 0b10}},
+    {3, 2, 1, 0, {0b100, 0b111}},
+    {2, 3, -1, 1, {0b1, 0b1, 0b11}},
+  },
+  { // O
+    {2, 2, 0, 0, {0b11, 0b11}},
+    {2, 2, 0, 0, {0b11, 0b11}},
+    {2, 2, 0, 0, {0b11, 0b11}},
+    {2, 2, 0, 0, {0b11, 0b11}},
+  },
+  { // S
+    {3, 2, 0, -1, {0b110, 0b011}},
+    {2, 3, 0, 0, {0b01, 0b11, 0b10}},
+    {3, 2, 1, 0, {0b110, 0b011}},
+    {2, 3, -1, 1, {0b01, 0b11, 0b10}},
+  },
+  { // T
+    {3, 2, 0, -1, {0b111, 0b010}},
+    {2, 3, 0, 0, {0b10, 0b11, 0b10}},
+    {3, 2, 1, 0, {0b010, 0b111}},
+    {2, 3, -1, 1, {0b1, 0b11, 0b1}},
+  },
+  { // Z
+    {3, 2, 0, -1, {0b011, 0b110}},
+    {2, 3, 0, 0, {0b10, 0b11, 0b01}},
+    {3, 2, 1, 0, {0b011, 0b110}},
+    {2, 3, -1, 1, {0b10, 0b11, 0b1}},
+  },
+};
+// clang-format on
+
+/**
+ * The Tetris game.
+ * This class is only responsible for the game logic. To interact with the user
+ * and the board, see TetrisApp.
+ */
+class TetrisGame {
+ private:
+  TetrisGameState state = GAME_STATE_PLAYING;
+  unsigned last_fall_time = 0;
+  int current_tetromino;
+  int current_x;
+  int current_y;
+  int current_rotation;
+
+  // board[0] is the top row, board[1] is the second row, etc.
+  // Each row is a byte, and each bit represents a cell.
+  // The LSB is the leftmost cell.
+  uint8_t board[BOARD_HEIGHT] = {0};
+  static_assert(BOARD_WIDTH == sizeof(uint8_t) * 8,
+                "TETRIS_BOARD_WIDTH must be 8");
+
+  unsigned (*rand)(void);
+
+  void clear_full_line();
+  bool rotate_tetromino();
+  bool place_tetromino(int x, int y);
+  bool place_tetromino(int x, int y, int rotation);
+  void unplace_tetromino(int x, int y);
+  void fall_down_tetromino();
+  bool generate_new_tetromino();
+  inline void game_over() { state = GAME_STATE_GAME_OVER; }
+
+ public:
+  TetrisGame(unsigned (*rand)(void));
+
+  void game_fall_down_tetromino();
+  void game_on_input(TetrisDirection direction);
+  void game_draw_to_display(display_buf_t *buf);
+  inline bool game_is_over() const { return state == GAME_STATE_GAME_OVER; };
+};
+
+}  // namespace tetris
+}  // namespace hitcon
+
+#endif  // TETRIS_GAME_H
