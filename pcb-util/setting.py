9c883c4350c89a9b7008958938cab6024e6485a1
Yen Lin <a870123tw@gmail.com>
2024-08-11 06:45:48 +0800

    feat: collect settings to setting.py

--- pcb-util/setting.py
+++ pcb-util/setting.py
@@ -4,6 +4,16 @@ Created on Sat Jul 13 08:16:40 2024
 
 @author: Arthur (Tora0615)
 """
+# Path
+## For ReplaceELF
+
+## For CLI tool
+FW_ELF_PATH = "C:\\Users\\a8701\\Documents\\Development\\hitcon-pcb-badge\\pcb-util\\fwMOD.elf"
+ST_PRO_PATH = (
+    "C:\\Program Files (x86)\\STMicroelectronics\\STM32Cube\\STM32CubeProgrammer\\bin"
+)
+ST_PRO_EXE = "STM32_Programmer_CLI.exe"
+
 
 # time relative (sec)
 THREAD_SLEEP_INTERVAL = 2
