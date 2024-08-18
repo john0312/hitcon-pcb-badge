f3c7c216f0efffe40c6cdc3237d1af0a177c29d6
doraeric <benson.doraemon@gmail.com>
2024-08-18 11:27:19 +0800

    feat: edit name backspace

--- fw/Core/Hitcon/Logic/Display/editor.h
+++ fw/Core/Hitcon/Logic/Display/editor.h
@@ -24,6 +24,7 @@ class TextEditorDisplay {
   void incr_current_char();
   void decr_current_char();
   void set_current_char(char c);
+  void backspace();
 
   void draw(uint8_t *buf, int frame) const;
   void draw_packed(display_buf_t *display_buf, int frame) const;
