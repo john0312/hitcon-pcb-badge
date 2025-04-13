72d02c59573c1ec8aa1a96257f9fc475101726f1
Tony Yang <t510599@gmail.com>
2025-04-13 15:26:05 +0800

    feat: connect packet_processor to app

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, APIRouter, Depends, Security, HTTPException
 from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+from pymongo import AsyncMongoClient
 from packet_processor import PacketProcessor
 from crypto_auth_layer import CryptoAuthLayer
 from game_logic import GameLogic
@@ -7,11 +8,15 @@ from config import Config
 from schemas import Station, IrPacketRequestSchema, Display
 
 config = Config("config.yaml")
+mongo = AsyncMongoClient(f"mongodb://{config['mongo']['username']}:{config['mongo']['password']}@{config['mongo']['host']}:{config['mongo']['port']}")
+db = mongo[config["mongo"]["db"]]
+stations = db["stations"]
+
 app = FastAPI()
 game_logic_instance = GameLogic(config=config)
 crypto_auth_layer_instance = CryptoAuthLayer(config=config)
 packet_processor_instance = PacketProcessor(
-    config=config, crypto_auth=crypto_auth_layer_instance
+    config=config, crypto_auth=crypto_auth_layer_instance, db=db
 )
 
 router = APIRouter(prefix="/v1")
@@ -20,11 +25,11 @@ security = HTTPBearer()
 async def get_station(credentials: HTTPAuthorizationCredentials = Security(security)) -> Station:
     key = credentials.credentials
 
-    # TODO: check key against database
-    # if key not in stations:
-    #     raise HTTPException(status_code=401, detail="Invalid key")
+    station = await stations.find_one({"station_key": key})
+    if station is None:
+        raise HTTPException(status_code=403, detail="Invalid station key")
 
-    return Station(station_id="1", station_key="key", display=Display(bar_1="bar1", bar_2="bar2", winning_color="red"), tx=[], rx=[])
+    return Station(**station)
 
 
 @app.get("/")
@@ -43,13 +48,21 @@ async def get_fake_scores():
 @router.get("/tx")
 async def tx(station: Station = Depends(get_station)) -> list[IrPacketRequestSchema]:
     # Backend asks the base station to send a packet.
-    pass
+    packets = await packet_processor_instance.has_packet_for_tx(station)
+
+    return list(packets)
 
 
 @router.post("/rx")
 async def rx(ir_packet: IrPacketRequestSchema, station: Station = Depends(get_station)):
     # Base station received a packet, sending it to the backend.
-    pass
+    try:
+        await packet_processor_instance.on_receive_packet(ir_packet, station)
+    except Exception as e:
+        print(f"Error processing packet: {e}")
+        raise HTTPException(status_code=500, detail=str(e))
+
+    return {"status": "ok"}
 
 
 @router.get("/station-display")
