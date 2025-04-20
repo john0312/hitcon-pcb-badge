ef84c86048ffd340093fd556598e9e700cb30fdc
Tony Yang <t510599@gmail.com>
2025-04-20 18:54:09 +0800

    update: schema

--- backend/schemas.py
+++ backend/schemas.py
@@ -1,10 +1,33 @@
-from typing import Optional, List
+from typing import Optional, List, Any, Callable
 from pydantic import BaseModel, Field, BeforeValidator
+from pydantic_core import core_schema
+from bson import ObjectId
 from typing import Annotated
 from enum import Enum
 import uuid
 
-PyObjectId = Annotated[str, BeforeValidator(str)]
+class _ObjectIdPydanticAnnotation:
+    # Based on https://docs.pydantic.dev/latest/usage/types/custom/#handling-third-party-types.
+
+    @classmethod
+    def __get_pydantic_core_schema__(
+        cls,
+        _source_type: Any,
+        _handler: Callable[[Any], core_schema.CoreSchema],
+    ) -> core_schema.CoreSchema:
+        def validate_from_str(input_value: str) -> ObjectId:
+            return ObjectId(input_value)
+
+        return core_schema.union_schema(
+            [
+                # check if it's an instance first before doing any further work
+                core_schema.is_instance_schema(ObjectId),
+                core_schema.no_info_plain_validator_function(validate_from_str),
+            ],
+            serialization=core_schema.to_string_ser_schema(),
+        )
+
+PyObjectId = Annotated[ObjectId, _ObjectIdPydanticAnnotation]
 PyBinary = Annotated[bytes, BeforeValidator(bytes)]
 
 PACKET_HASH_LEN = 6
@@ -26,7 +49,7 @@ class IrPacket(BaseModel):
     packet_id: Optional[uuid.UUID]
     # The packet_id to avoid duplication at base station.
     data: bytes
-    station_id: uuid.UUID
+    station_id: Optional[uuid.UUID]
     to_stn: bool
     # to_stn is True for backend -> base station packet, False otherwise.
 
@@ -34,7 +57,7 @@ class IrPacket(BaseModel):
 # For http requests
 class IrPacketRequestSchema(BaseModel):
     packet_id: Optional[uuid.UUID]
-    data: bytes
+    data: List[int]
 
 
 # For Mongo
@@ -43,6 +66,7 @@ class IrPacketObject(BaseModel):
     packet_id: Optional[uuid.UUID]
     data: PyBinary
     hash: PyBinary
+    timestamp: int
 
 
 class ActivityEvent(BaseModel):
