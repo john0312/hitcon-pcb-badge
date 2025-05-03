b1d60b391ac75b1f6ca9899ffed3febbdeefb0e1
aoaaceai <aoaaceai@csie.ntu.edu.tw>
2025-05-03 16:55:46 +0800

    Add stub for hash service

---
+++ fw/Core/Hitcon/Service/HashService.cc
@@ -0,0 +1,10 @@
+#include <Service/HashService.h>
+
+namespace hitcon {
+
+bool HashService::StartHash(uint8_t const *message, size_t len,
+                            callback_t callback, void *callback_arg1) {
+  return false;
+}
+
+}  // namespace hitcon
