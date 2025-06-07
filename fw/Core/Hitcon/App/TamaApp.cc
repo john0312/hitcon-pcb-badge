f7130a0ef5c51691fc7757c628d70c03e0d12658
William Tsai <williamtsai1111@gmail.com>
2025-06-07 21:05:27 +0800

    Create basic framework for Tama App

--- fw/Core/Hitcon/App/TamaApp.cc
+++ fw/Core/Hitcon/App/TamaApp.cc
@@ -1,88 +1,258 @@
 #include "TamaApp.h"
 
 #include <Logic/BadgeController.h>
+#include <Logic/Display/display.h>
 #include <Logic/NvStorage.h>
 #include <Service/Sched/Scheduler.h>
+
+#include <cstring>  // For memset if needed, though direct assignment is used
 
 namespace hitcon {
 namespace app {
 namespace tama {
+
 TamaApp tama_app;
 
 TamaApp::TamaApp()
-    : _routine_task(930,
+    : _routine_task(930,  // Task priority
                     (hitcon::service::sched::task_callback_t)&TamaApp::Routine,
-                    (void*)this, INTERVAL),
-      _tama_data(g_nv_storage.GetCurrentStorage().tama_storage) {}
+                    (void*)this, ROUTINE_INTERVAL_MS),
+      _tama_data(g_nv_storage.GetCurrentStorage().tama_storage),
+      _current_selection_in_choose_mode(TAMA_TYPE::DOG), _fb() {}
 
 void TamaApp::Init() {
   hitcon::service::sched::scheduler.Queue(&_routine_task, nullptr);
+  // _tama_data is loaded from NvStorage.
+  // If it's a fresh start (e.g., NvStorage is zeroed), _tama_data.type will be
+  // 0 (NONE_TYPE).
+  // use new data always for debugging
+  _tama_data = {};
+  g_nv_storage.MarkDirty();
 }
 
 void TamaApp::OnEntry() {
-  display_buf_t frame_buf[DISPLAY_WIDTH] = {0};
+  hitcon::service::sched::scheduler.EnablePeriodic(&_routine_task);
 
-  // Draw the dog bitmap (columns 0-7 of frame_buf)
-  // 'i' will be the column index within the 8x8 bitmap (0 to 7)
-  // 'j' will be the row index within the 8x8 bitmap (0 to 7)
-  for (uint8_t i = 0; i < 8; i++) {
-    for (uint8_t j = 0; j < 8; j++) {
-      // Get the pixel value (0 or 1) from the dog bitmap.
-      // tama_dog_bitmap[j] is the data for row 'j'.
-      // (7 - i) accesses the i-th pixel from the left (MSB is pixel 0, LSB is pixel 7).
-      uint8_t pixel_value = display_buf_get(tama_dog_bitmap[j], 7 - i);
-
-      if (pixel_value) {
-        // If the pixel is set, set the corresponding bit in the display buffer.
-        // frame_buf[i] is the i-th column of the display.
-        // (1 << j) creates a mask for the j-th row.
-        frame_buf[i] |= (1 << j);
-      }
-    }
+  if (_tama_data.state == TAMA_APP_STATE::CHOOSE_TYPE) {
+    // Ensure _current_selection_in_choose_mode is valid. Default to DOG if type
+    // is NONE.
+  } else if (_tama_data.state == TAMA_APP_STATE::INTRO_TEXT) {
+    // If OnEntry is called while still in INTRO_TEXT, ensure scrolling text is
+    // set.
+    display_set_mode_scroll_text("Choose your pet");
   }
-
-  // Draw the cat bitmap (columns 8-15 of frame_buf)
-  // 'i' will be the column index within the 8x8 bitmap (0 to 7)
-  // 'j' will be the row index within the 8x8 bitmap (0 to 7)
-  for (uint8_t i = 0; i < 8; i++) {
-    uint8_t display_column = 8 + i; // Offset for the cat's position
-    if (display_column >= DISPLAY_WIDTH) {
-        // Avoid writing out of bounds if display is not wide enough
-        break;
-    }
-    for (uint8_t j = 0; j < 8; j++) {
-      // Get the pixel value (0 or 1) from the cat bitmap.
-      uint8_t pixel_value = display_buf_get(tama_cat_bitmap[j], 7 - i);
-
-      if (pixel_value) {
-        // Set the corresponding bit in the display buffer for the cat.
-        // frame_buf[display_column] is the target column for the cat.
-        // (1 << j) creates a mask for the j-th row.
-        frame_buf[display_column] |= (1 << j);
-      }
-    }
-  }
-  display_set_mode_fixed_packed(frame_buf);
-
 }
 
-void TamaApp::OnExit() {}
+void TamaApp::OnExit() {
+  hitcon::service::sched::scheduler.DisablePeriodic(&_routine_task);
+  // NvStorage will be flushed by the system if MarkDirty was called.
+}
+
+void TamaApp::Render() {
+  if (_tama_data.state == TAMA_APP_STATE::INTRO_TEXT) {
+    // For INTRO_TEXT, display_set_mode_scroll_text() is called in OnEntry.
+    // The display system handles rendering for this mode.
+    // This Render() function should not interfere by setting fixed_packed mode.
+    return;
+  }
+
+  // Ensure fb_size is valid to prevent division by zero.
+  // The constructor should have initialized this.
+  if (_fb.fb_size == 0) {
+    return;
+  }
+
+  display_buf_t* current_screen_buffer = _fb.fb[_fb.active_frame];
+  display_set_mode_fixed_packed(current_screen_buffer);
+  _fb.active_frame = (_fb.active_frame + 1) % _fb.fb_size;
+}
 
 void TamaApp::OnButton(button_t button) {
+  bool needs_save = false;
+  bool needs_update_fb = false;
+
   switch (button & BUTTON_VALUE_MASK) {
     case BUTTON_BACK:
     case BUTTON_LONG_BACK:
       badge_controller.BackToMenu(this);
+      return;  // Exit immediately
+
+    case BUTTON_OK:
+      switch (_tama_data.state) {
+        case TAMA_APP_STATE::INTRO_TEXT:
+          break;
+        case TAMA_APP_STATE::CHOOSE_TYPE:
+          _tama_data.type = _current_selection_in_choose_mode;
+          _tama_data.state = TAMA_APP_STATE::EGG;
+          needs_update_fb = true;
+          needs_save = true;
+          break;
+        case TAMA_APP_STATE::EGG:
+          // This allows pressing OK on the egg to hatch it immediately.
+          _tama_data.state = TAMA_APP_STATE::ALIVE;
+          needs_update_fb = true;
+          needs_save = true;
+          break;
+        default:
+          // No action for other states on OK press, or handle as needed
+          break;
+      }
       break;
+    case BUTTON_LEFT:
+      switch (_tama_data.state) {
+        case TAMA_APP_STATE::CHOOSE_TYPE:
+          if (_current_selection_in_choose_mode == TAMA_TYPE::CAT) {
+            needs_update_fb = true;
+          }
+          _current_selection_in_choose_mode = TAMA_TYPE::DOG;
+          break;
+          // TODO: Handle other states for BUTTON_LEFT if necessary
+        default:
+          break;
+      }
+      break;
+    case BUTTON_RIGHT:
+      switch (_tama_data.state) {
+        case TAMA_APP_STATE::CHOOSE_TYPE:
+          if (_current_selection_in_choose_mode == TAMA_TYPE::DOG) {
+            needs_update_fb = true;
+          }
+          _current_selection_in_choose_mode = TAMA_TYPE::CAT;
+          break;
+          // TODO: Handle other states for BUTTON_LEFT if necessary
+        default:
+          break;
+      }
     default:
       break;
   }
-  return;
+
+  if (needs_save) {
+    g_nv_storage.MarkDirty();
+  }
+  if (needs_update_fb) {
+    UpdateFrameBuffer();
+    Render();
+  }
 }
 
 void TamaApp::OnEdgeButton(button_t button) {}
 
-void TamaApp::Routine(void* unused) {}
+void TamaApp::Routine(void* unused) {
+  bool needs_render = true;
+  bool needs_save = false;
+
+  switch (_tama_data.state) {
+    case TAMA_APP_STATE::INTRO_TEXT:
+      needs_render = false;
+      if (display_get_scroll_count() >= 1) {
+        _tama_data.state = TAMA_APP_STATE::CHOOSE_TYPE;
+        needs_render = true;
+        UpdateFrameBuffer();
+      }
+      break;
+    default:
+      break;
+  }
+
+  if (needs_save) {
+    g_nv_storage.MarkDirty();
+  }
+
+  if (needs_render) {
+    // Only render if this app is currently active.
+    // This check might be redundant if BadgeController ensures OnEntry/OnExit
+    // are paired correctly with task enabling/disabling, but it's a safe check.
+    if (hitcon::badge_controller.GetCurrentApp() == this) {
+      Render();
+    }
+  }
+}
+void TamaApp::UpdateFrameBuffer() {
+  switch (_tama_data.state) {
+    case TAMA_APP_STATE::CHOOSE_TYPE:
+      const tama_ani_t* selected_animation;
+      if (_current_selection_in_choose_mode == TAMA_TYPE::DOG) {
+        selected_animation =
+            &animation[static_cast<uint8_t>(TAMA_ANIMATION_TYPE::DOG)];
+      } else if (_current_selection_in_choose_mode == TAMA_TYPE::CAT) {
+        selected_animation =
+            &animation[static_cast<uint8_t>(TAMA_ANIMATION_TYPE::CAT)];
+      } else {
+        hitcon::service::sched::my_assert(
+            false);  // Should not happen if state is CHOOSE_TYPE
+      }
+
+      _fb.fb_size = selected_animation->frame_count;
+      _fb.active_frame = 0;  // Start from the first frame
+      switch (_current_selection_in_choose_mode) {
+        case TAMA_TYPE::DOG:
+          // Copy DOG animation frames to left
+          for (int i = 0; i < _fb.fb_size; ++i) {
+            memset(_fb.fb[i], 0, sizeof(display_buf_t) * DISPLAY_WIDTH);
+            memcpy(_fb.fb[i], selected_animation->frames_data[i],
+                   sizeof(display_buf_t[8]));
+            // Draw right arrow
+            for (int col = 0; col < BITMAP_RIGHT_ARROW_WIDTH; ++col) {
+              _fb.fb[i][DISPLAY_WIDTH - 1 - col] |=
+                  bitmap_right_arrow_cols[BITMAP_RIGHT_ARROW_WIDTH - 1 - col];
+            }
+          }
+          break;
+        case TAMA_TYPE::CAT:
+          // Copy CAT animation frames to right
+          for (int i = 0; i < _fb.fb_size; ++i) {
+            memset(_fb.fb[i], 0, sizeof(display_buf_t) * DISPLAY_WIDTH);
+            memcpy(_fb.fb[i] + (DISPLAY_WIDTH - sizeof(display_buf_t[8])),
+                   selected_animation->frames_data[i],
+                   sizeof(display_buf_t[8]));
+            // Draw left arrow
+            for (int col = 0; col < BITMAP_LEFT_ARROW_WIDTH; ++col) {
+              _fb.fb[i][col] |= bitmap_left_arrow_cols[col];
+            }
+          }
+      }
+      break;
+    case TAMA_APP_STATE::EGG:
+      // Draw egg animation in the cneter
+      selected_animation =
+          &animation[static_cast<uint8_t>(TAMA_ANIMATION_TYPE::EGG)];
+      _fb.fb_size = selected_animation->frame_count;
+      _fb.active_frame = 0;  // Start from the first frame
+      for (int i = 0; i < _fb.fb_size; ++i) {
+        // Center the egg animation (8 columns wide)
+        memset(_fb.fb[i], 0, sizeof(display_buf_t) * DISPLAY_WIDTH);
+        memcpy(_fb.fb[i] + (DISPLAY_WIDTH / 2 - 4), TAMA_EGG_FRAMES[i],
+               sizeof(display_buf_t[8]));
+      }
+      break;
+    case TAMA_APP_STATE::ALIVE:
+      // Draw pet animation in the center
+      if (_tama_data.type == TAMA_TYPE::DOG) {
+        selected_animation =
+            &animation[static_cast<uint8_t>(TAMA_ANIMATION_TYPE::DOG)];
+      } else if (_tama_data.type == TAMA_TYPE::CAT) {
+        selected_animation =
+            &animation[static_cast<uint8_t>(TAMA_ANIMATION_TYPE::CAT)];
+      } else {
+        // Should not happen if state is ALIVE
+        hitcon::service::sched::my_assert(false);
+      }
+      _fb.fb_size = selected_animation->frame_count;
+      _fb.active_frame = 0;  // Start from the first frame
+      for (int i = 0; i < _fb.fb_size; ++i) {
+        // Center the pet animation (8 columns wide)
+        memset(_fb.fb[i], 0, sizeof(display_buf_t) * DISPLAY_WIDTH);
+        memcpy(_fb.fb[i] + (DISPLAY_WIDTH / 2 - 4),
+               selected_animation->frames_data[i], sizeof(display_buf_t[8]));
+      }
+      break;
+    default:
+      // Should not happen in CHOOSE_TYPE state
+      hitcon::service::sched::my_assert(false);
+      break;
+  }
+}
 }  // namespace tama
 }  // namespace app
 }  // namespace hitcon
--- fw/Core/Hitcon/App/TamaApp.h
+++ fw/Core/Hitcon/App/TamaApp.h
@@ -3,6 +3,7 @@
 
 #include <Logic/Display/display.h>
 #include <Service/Sched/PeriodicTask.h>
+#include <Service/Sched/SysTimer.h>  // For SysTimer
 
 #include "app.h"
 
@@ -10,27 +11,50 @@ namespace hitcon {
 namespace app {
 namespace tama {
 
-enum TAMA_APP_STATE {
-  CHOOSE_TYPE,
-  EGG,
+enum class TAMA_APP_STATE : uint8_t {
+  INTRO_TEXT,   // Displaying introductory text
+  CHOOSE_TYPE,  // Player is selecting a pet type
+  EGG,          // Pet is an egg, waiting to hatch
+  ALIVE,        // Pet has hatched and is alive
+  // TODO: Add states like DEAD, EVOLVING, etc.
 };
 
-enum TAMA_TYPE {
-  NONE,
-  DOG,
-  CAT,
+enum class TAMA_TYPE : uint8_t {
+  NONE_TYPE,  // No pet selected or game reset
+  DOG,        // Player chose a dog
+  CAT,        // Player chose a cat
 };
 
 typedef struct {
   TAMA_APP_STATE state;
   TAMA_TYPE type;
+  unsigned int state_enter_time_ms;  // Timestamp when the current state (e.g.,
+                                     // EGG) was entered
+  // TODO: Add more stats for ALIVE state:
+  // uint32_t birth_time_ms; // Actual hatch time
+  // uint8_t hunger;
+  // uint8_t happiness;
+  // uint32_t last_interaction_time_ms;
 } tama_storage_t;
+
+typedef struct {
+  display_buf_t fb[12][DISPLAY_WIDTH];
+  uint8_t fb_size;
+  uint8_t active_frame;
+} tama_display_fb_t;
 
 class TamaApp : public App {
  private:
-  static constexpr unsigned INTERVAL = 150;
+  static constexpr unsigned ROUTINE_INTERVAL_MS =
+      500;  // How often the Routine function runs
+  TAMA_TYPE _current_selection_in_choose_mode;
   hitcon::service::sched::PeriodicTask _routine_task;
   tama_storage_t& _tama_data;
+  tama_display_fb_t _fb;
+
+  void Render();
+  void Routine(void* unused);
+  void UpdateFrameBuffer();
 
  public:
   TamaApp();
@@ -41,35 +65,142 @@ class TamaApp : public App {
   void OnExit() override;
   void OnButton(button_t button) override;
   void OnEdgeButton(button_t button) override;
-  void Routine(void* unused);
 };
 
 extern TamaApp tama_app;
 
+// --- Animation Frame Data Definitions (for tama_ani_t and static asserts) ---
+// Each frame is 8 columns wide. Characters are 5 columns wide and centered.
+
+constexpr display_buf_t TAMA_DOG_FRAMES[2][8] = {
+    {
+        /*
+         * code=68, hex=0x44, ascii="D"
+         */
+        0xf0, /* 111100 */
+        0x88, /* 100010 */
+        0x88, /* 100010 */
+        0x88, /* 100010 */
+        0x88, /* 100010 */
+        0x88, /* 100010 */
+        0xf0, /* 111100 */
+        0x00, /* 000000 */
+    },
+    {
+        /*
+         * code=71, hex=0x47, ascii="G"
+         */
+        0x70, /* 011100 */
+        0x88, /* 100010 */
+        0x80, /* 100000 */
+        0x98, /* 100110 */
+        0x88, /* 100010 */
+        0x88, /* 100010 */
+        0x70, /* 011100 */
+        0x00, /* 000000 */
+    }};
+constexpr display_buf_t TAMA_CAT_FRAMES[2][8] = {
+    {
+        /*
+         * code=67, hex=0x43, ascii="C"
+         */
+        0x70, /* 011100 */
+        0x88, /* 100010 */
+        0x80, /* 100000 */
+        0x80, /* 100000 */
+        0x80, /* 100000 */
+        0x88, /* 100010 */
+        0x70, /* 011100 */
+        0x00, /* 000000 */
+    },
+    {
+        /*
+         * code=84, hex=0x54, ascii="T"
+         */
+        0xf8, /* 111110 */
+        0x20, /* 001000 */
+        0x20, /* 001000 */
+        0x20, /* 001000 */
+        0x20, /* 001000 */
+        0x20, /* 001000 */
+        0x20, /* 001000 */
+        0x00, /* 000000 */
+    }};
+constexpr display_buf_t TAMA_EGG_FRAMES[2][8] = {
+    {0x00, 0x3C, 0x7E, 0xFF, 0xFF, 0x7E, 0x3C, 0x00},
+    {0x00, 0x38, 0x7C, 0xFE, 0xFE, 0x7C, 0x38, 0x00}};
+constexpr display_buf_t TAMA_HEART_FRAMES[1][8] = {
+    {0b00000000, 0b01100110, 0b11111111, 0b11111111, 0b01111110, 0b00111100,
+     0b00011000, 0b00000000}};
+
+// --- Animation Definition Structure --
+
+enum class TAMA_ANIMATION_TYPE : uint8_t { DOG, CAT, EGG, HEART };
+typedef struct {
+  TAMA_ANIMATION_TYPE type;
+  int frame_count;
+  const display_buf_t (*frames_data)[8];
+} tama_ani_t;
+
+constexpr tama_ani_t animation[] = {
+    {.type = TAMA_ANIMATION_TYPE::DOG,
+     .frame_count = 2,
+     .frames_data = TAMA_DOG_FRAMES},
+    {.type = TAMA_ANIMATION_TYPE::CAT,
+     .frame_count = 2,
+     .frames_data = TAMA_CAT_FRAMES},
+    {.type = TAMA_ANIMATION_TYPE::EGG,
+     .frame_count = 2,
+     .frames_data = TAMA_EGG_FRAMES},
+    {.type = TAMA_ANIMATION_TYPE::HEART,
+     .frame_count = 1,
+     .frames_data = TAMA_HEART_FRAMES},
+};
+
+// Macro to check animation properties
+#define ASSERT_ANIMATION_PROPERTIES(TYPE_NAME_STR)                            \
+  static_assert(                                                              \
+      animation[static_cast<uint8_t>(TAMA_ANIMATION_TYPE::TYPE_NAME_STR)]     \
+              .type == TAMA_ANIMATION_TYPE::TYPE_NAME_STR,                    \
+      #TYPE_NAME_STR " Animation type mismatch");                             \
+  static_assert(                                                              \
+      animation[static_cast<uint8_t>(TAMA_ANIMATION_TYPE::TYPE_NAME_STR)]     \
+              .frame_count ==                                                 \
+          (sizeof(TAMA_##TYPE_NAME_STR##_FRAMES) / sizeof(display_buf_t[8])), \
+      #TYPE_NAME_STR " Frame count mismatch")
+
+// Using the macro for static asserts
+ASSERT_ANIMATION_PROPERTIES(DOG);
+ASSERT_ANIMATION_PROPERTIES(CAT);
+ASSERT_ANIMATION_PROPERTIES(EGG);
+ASSERT_ANIMATION_PROPERTIES(HEART);
+
+// Left Arrow '<' (2 columns wide, 3 pixels high: rows 3,4,5)
+/* Visualized (Row-major, '.' is off, 'X' is on, for the arrow part):
+..X. (col 0, row 4)
+.X.. (col 1, row 3)
+.X.. (col 1, row 5)
+*/
+constexpr int BITMAP_LEFT_ARROW_WIDTH = 2;
+constexpr display_buf_t bitmap_left_arrow_cols[BITMAP_LEFT_ARROW_WIDTH] = {
+    0b00010000,  // Column 0: pixel at row 4
+    0b00101000   // Column 1: pixels at row 3 and 5
+};
+
+// Right Arrow '>' (2 columns wide, 3 pixels high: rows 3,4,5)
+/* Visualized (Row-major, '.' is off, 'X' is on, for the arrow part):
+.X.. (col 0, row 3)
+.X.. (col 0, row 5)
+..X. (col 1, row 4)
+*/
+constexpr int BITMAP_RIGHT_ARROW_WIDTH = 2;
+constexpr display_buf_t bitmap_right_arrow_cols[BITMAP_RIGHT_ARROW_WIDTH] = {
+    0b00101000,  // Column 0: pixels at row 3 and 5
+    0b00010000   // Column 1: pixel at row 4
+};
+
 }  // namespace tama
 }  // namespace app
 }  // namespace hitcon
-
-constexpr display_buf_t tama_dog_bitmap[8] = {
-    0b11100111,
-    0b00111100,
-    0b01000010,
-    0b10000001,
-    0b10100101,
-    0b10000001,
-    0b01000010,
-    0b00111100,
-};
-
-constexpr display_buf_t tama_cat_bitmap[8] = {
-    0b01000010,
-    0b10100101,
-    0b10011001,
-    0b10000001,
-    0b10100101,
-    0b10000001,
-    0b01000010,
-    0b00111100,
-};
 
 #endif  // TAMA_APP_H
