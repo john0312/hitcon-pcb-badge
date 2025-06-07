a68f047c7a0e3e65011aa8f94033b45935517969
Tony Yang <t510599@gmail.com>
2025-06-07 17:07:10 +0800

    feat(GameLogicController): connect PacketProcessor with GameLogic

--- backend/game_logic_controller.py
+++ backend/game_logic_controller.py
@@ -1,13 +1,40 @@
 from schemas import ProximityEvent, PubAnnounceEvent, TwoBadgeActivityEvent, GameActivityEvent, SingleBadgeActivityEvent, SponsorActivityEvent
+from database import mongo, db
 from game_logic import _GameLogic as GameLogic, GameType
+import uuid
 
-game = GameLogic()
+game = GameLogic(mongo)
 
 class GameLogicController:
     # ===== APIs for PacketProcessor =====
     @staticmethod
     async def on_single_badge_activity_event(evt: SingleBadgeActivityEvent):
-        pass
+        # GameType
+        # 0x01 - Snake
+        # 0x02 - Tetris
+        # 0x03 - Dino
+        # 0x10 - Shake
+        match evt.event_type:
+            case 0x01:
+                game_type = GameType.SNAKE
+            case 0x02:
+                game_type = GameType.TETRIS
+            case 0x03:
+                game_type = GameType.DINO
+            case 0x10:
+                game_type = GameType.SHAKE_BADGE
+            case _:
+                raise ValueError(f"Unknown game type: {evt.event_type}")
+
+        await game.receive_game_score(
+            player_id=evt.user,
+            station_id=evt.station_id,
+            event_data=evt.event_data,
+            game_type=game_type,
+            timestamp=evt.timestamp
+        )
+
+        return evt.user
 
 
     @staticmethod
@@ -27,9 +54,29 @@ class GameLogicController:
 
     @staticmethod
     async def on_proximity_event(evt: ProximityEvent):
-        print("Event", evt)
+        await game.attack_station(
+            player_id=evt.user,
+            station_id=evt.station_id,
+            amount=evt.power,
+            timestamp=evt.timestamp
+        )
+
+        return evt.user
 
 
     @staticmethod
     async def on_pub_announce_event(evt: PubAnnounceEvent):
+        print(f"Pub announce event: {evt.pubkey.hex()}")
+        print(f"Signature: {evt.signature.hex()}")
+        # station <--> user has been recorded by the PacketProcessor
         pass
+
+
+    @staticmethod
+    async def get_user_score():
+        return await game.get_game_score()
+
+
+    @staticmethod
+    async def get_station_score(station_id: uuid.UUID):
+        return await game.get_station_score(station_id)
