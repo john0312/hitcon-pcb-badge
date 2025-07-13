abbacf7fcb6d95d7d6cea003eed5d6cec7c04388
Lance798 <hz946128@gmail.com>
2025-07-13 17:18:28 +0800

    Update Hitcon.h

--- fw/Core/Hitcon/Hitcon.h
+++ fw/Core/Hitcon/Hitcon.h
@@ -10,7 +10,8 @@
 
 // V1_1C  - HITCON CMT 2024 final version
 // V2_0A  - HITCON CMT 2025 first prototype
-// V2_1B - HITCON CMT 2025 second prototype (blue light version)
+// V2_1BB - HITCON CMT 2025 second prototype (blue light version)
+// V2_1BR - HITCON CMT 2025 second prototype (red light version)
 #if !defined(V1_1C) && !defined(V2_0A) && !defined(V2_1B)
 #error "You must define at least one of: V1_1C, V2_0A, V2_1B"
 #endif
