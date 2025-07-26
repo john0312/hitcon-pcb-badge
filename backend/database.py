4b33d45ce83095766e882cf3b83850ebcb73d03b
Tony Yang <t510599@gmail.com>
2025-07-26 21:32:38 +0800

    fix: mongo authentication failed

--- backend/database.py
+++ backend/database.py
@@ -4,7 +4,7 @@ from config import Config
 
 config = Config("config.yaml")
 
-mongo = AsyncMongoClient(f"mongodb://{config['mongo']['username']}:{config['mongo']['password']}@{config['mongo']['host']}:{config['mongo']['port']}?uuidRepresentation=standard")
+mongo = AsyncMongoClient(f"mongodb://{config['mongo']['host']}:{config['mongo']['port']}?uuidRepresentation=standard")
 db = mongo[config["mongo"]["db"]]
 
 redis_client = redis.Redis(
