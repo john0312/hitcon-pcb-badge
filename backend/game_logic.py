8792a76c5326e6972da99a706399a12c8d2a4fa8
Tony Yang <t510599@gmail.com>
2025-07-26 19:30:01 +0800

    fix: apply palyer buff function prototype

--- backend/game_logic.py
+++ backend/game_logic.py
@@ -339,7 +339,7 @@ class _GameLogic:
         return score
 
 
-    async def apply_player_buff(player_id: int, buff_a_count: int, buff_b_count: int, timestamp: datetime):
+    async def apply_player_buff(self, player_id: int, buff_a_count: int, buff_b_count: int, timestamp: datetime):
         """
         Apply a buff to the player.
         buff_a and buff_b has different parameter on the modifier.
