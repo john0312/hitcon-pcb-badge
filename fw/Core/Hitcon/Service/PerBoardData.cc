939c1e5d0416cf7acbc3b37f45a579738ca66a10
aoaaceai <aoaaceai@csie.ntu.edu.tw>
2025-08-07 00:51:10 +0800

    Make per-board privkey binary

--- fw/Core/Hitcon/Service/PerBoardData.cc
+++ fw/Core/Hitcon/Service/PerBoardData.cc
@@ -1,4 +1,5 @@
 #include <Service/PerBoardData.h>
+#include <cstring>
 
 namespace hitcon {
 
@@ -16,7 +17,10 @@ constexpr uint8_t kPubKeyCert[PerBoardData::kPubKeyCertSize] = {
     0x7d, 0xf0, 0xde, 0x4c, 0xe2, 0x23, 0x19,
     0xf6, 0xb4, 0xfa, 0xbe, 0x12, 0x6d, 0x41};
 
-constexpr uint64_t kPrivKey = 13447439586820736;
+constexpr uint8_t kPrivKey[PerBoardData::kPrivKeySize] = {
+    0x80, 0x02, 0xb6, 0x03, 0x60, 0xc6, 0x2f
+};
+
 }  // namespace
 
 constexpr PerBoardData::PerBoardData() {}
@@ -27,5 +31,11 @@ const uint8_t* PerBoardData::GetPerBoardSecret() { return kPerBoardSecret; }
 
 const uint8_t* PerBoardData::GetPubKeyCert() { return kPubKeyCert; }
 
-const uint64_t PerBoardData::GetPrivKey() { return kPrivKey; }
+const uint64_t PerBoardData::GetPrivKey() {
+    uint64_t privkey = 0;
+    memcpy(&privkey, kPrivKey, PerBoardData::kPrivKeySize);
+    return privkey;
+}
+
+
 }  // namespace hitcon
