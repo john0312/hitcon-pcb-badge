b2185c90bf9e775dad1b315c74f35f702dc3032e
aoaaceai <aoaaceai@csie.ntu.edu.tw>
2025-08-09 16:35:56 +0800

    Handle userid conflicts

--- pcb-util/server/backend/database.py
+++ pcb-util/server/backend/database.py
@@ -3,14 +3,15 @@ from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorDatabase
 from typing import Any, Dict, Annotated, Optional
 import asyncio
 from pymongo.errors import DuplicateKeyError
+import ecc
 
 class PrivKeyExistsError(Exception):
     pass
 
 class BoardData(Document):
     board_secret: bytes
-    priv_key: Annotated[bytes, Indexed(unique=True)]
-
+    priv_key: bytes
+    user_id: Annotated[bytes, Indexed(unique=True)]
 
     def __repr__(self) -> str:
         return f"BoardData(board_secret={self.board_secret}, priv_key={self.priv_key}, id={self.id})"
@@ -37,21 +38,9 @@ class Storage:
         self.client.close()
 
 async def store_item(board_secret: bytes, priv_key: bytes) -> Optional[BoardData]:
-    data = BoardData(board_secret=board_secret, priv_key=priv_key)
+    data = BoardData(board_secret=board_secret, priv_key=priv_key, user_id=ecc.privkey_to_username(priv_key))
     try:
         return await data.insert()
     except DuplicateKeyError:
         raise PrivKeyExistsError("Private key already exists in the database")
-
-async def main():
-    storage: Storage = await Storage.create("mongodb://localhost:27017", "board_logs")
-    try:
-        stored_item = await store_item(b'aoeu', b'htns')
-        print(stored_item)
-    finally:
-        await storage.close()
-
-
-if __name__ == '__main__':
-    asyncio.run(main())
 
