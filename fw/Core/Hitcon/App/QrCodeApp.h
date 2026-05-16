5a394830eb098ee8d3f610d541a50a73b88bf5a0
doraeric <benson.doraemon@gmail.com>
2026-05-16 19:52:40 +0800

    feat(fw): add QR Code display app

---
+++ fw/Core/Hitcon/App/QrCodeApp.h
@@ -0,0 +1,27 @@
+#ifndef HITCON_APP_QR_CODE_APP_H_
+#define HITCON_APP_QR_CODE_APP_H_
+
+#include <App/app.h>
+
+namespace hitcon {
+
+class QrCodeApp : public App {
+ public:
+  QrCodeApp() = default;
+  virtual ~QrCodeApp() = default;
+
+  void OnEntry() override;
+  void OnExit() override {}
+  void OnButton(button_t button) override;
+
+ private:
+  int offset_x_ = 0;
+  int offset_y_ = 0;
+  void Render();
+};
+
+extern QrCodeApp qr_code_app;
+
+}  // namespace hitcon
+
+#endif  // HITCON_APP_QR_CODE_APP_H_
